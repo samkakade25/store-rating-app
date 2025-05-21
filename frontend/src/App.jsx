db2650eb51f577c2fc4adf041531cb2343924037
sameer <rkvfc89@gmail.com>
2025-05-21 22:59:18 +0530

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="font-extrabold text-8xl bg-amber-500 p-10 m-4">
+        Hello sameer
+      </div>
+    </>
+  )
+}
+
+export default App
